2e9ca78de01026b3a7d86584ba6dff16bc20816f
Jongmin Won <jacobjwon@gmail.com>
2018-01-02 15:15:00 -0500

    More project setup checklist

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/entryfile.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
