c017738e14cd162a83dc33b309c9b6b8aaab6447
Jongmin Won <jacobjwon@gmail.com>
2018-01-03 15:19:57 -0500

    Add functionality for user login, sign up, and sign out and delete email column from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102191342) do
+ActiveRecord::Schema.define(version: 20180103193457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
-    t.string "email", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
     t.text "bio"
@@ -25,7 +24,6 @@ ActiveRecord::Schema.define(version: 20180102191342) do
     t.string "cover_img_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -53,27 +53,29 @@ class SessionForm extends React.Component {
     return (
       <div className="login-form-container">
         <form onSubmit={this.handleSubmit} className="login-form-box">
-          Welcome to FiveHundredDMX!
+          Log In to 500dmx
           <br/>
           Please {this.props.formType} or {this.navLink()}
           {this.renderErrors()}
           <div className="login-form">
             <br/>
-            <label>Username:
+              <label>Username
+              </label>
+            <br/>
               <input type="text"
                 value={this.state.username}
                 onChange={this.update('username')}
                 className="login-input"
               />
-            </label>
             <br/>
-            <label>Password:
+              <label>Password
+              </label>
+            <br/>
               <input type="password"
                 value={this.state.password}
                 onChange={this.update('password')}
                 className="login-input"
               />
-            </label>
             <br/>
             <input type="submit" value="Submit" />
           </div>
