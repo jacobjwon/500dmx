319c45a931810e67113866e10f9358195eeae8bf
Jongmin Won <jacobjwon@gmail.com>
2018-01-03 12:27:31 -0500

    Work on greetings/session components and route_util

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -8,8 +8,8 @@ import {
   HashRouter
 } from 'react-router-dom';
 
-// import GreetingContainer from './greeting/greeting_container';
-// import SessionFormContainer from './session_form/session_form_container';
+import GreetingContainer from './greeting/greeting_container';
+import SessionFormContainer from './session_form/session_form_container';
 // import SearchContainer from './search/search_container';
 // import BenchShowContainer from './bench_show/bench_show_container';
 // import BenchFormContainer from './bench_form/bench_form_container';
@@ -18,19 +18,24 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 const App = () => (
   <div>
     <header>
-      // <Link to="/" className="header-link">
-        <h1>Bench BnB</h1>
-      // </Link>
-      // <GreetingContainer />
+        <h1>500dmx</h1>
+      <GreetingContainer />
     </header>
-    // <Switch>
-      // <AuthRoute path="/login" component={SessionFormContainer} />
-      // <AuthRoute path="/signup" component={SessionFormContainer} />
-      // <ProtectedRoute path="/benches/new" component={BenchFormContainer} />
-      // <Route path="/benches/:benchId" component={BenchShowContainer} />
-      // <Route exact path="/" component={SearchContainer} />
-    // </Switch>
+      <AuthRoute path="/login" component={SessionFormContainer} />
+      <AuthRoute path="/signup" component={SessionFormContainer} />
   </div>
 );
+
+// this link wraps the header above
+// <Link to="/" className="header-link">
+// </Link>
+
+// this switch wraps the auth routes below the header above
+// <Switch>
+// these 3 belong within switch above
+// <ProtectedRoute path="/benches/new" component={BenchFormContainer} />
+// <Route path="/benches/:benchId" component={BenchShowContainer} />
+// <Route exact path="/" component={SearchContainer} />
+// </Switch>
 
 export default App;
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -6,17 +6,15 @@ import configureStore from './store/store';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  // let store;
-  // if (window.currentUser) {
-  //   const preloadedState = { session: { currentUser: window.currentUser } };
-  //   store = configureStore(preloadedState);
-  //   delete window.currentUser;
-  // } else {
-  //   store = configureStore();
-  // }
-  const store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
