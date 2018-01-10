ac6a9bea3c8d61ec2c5ab25392dfe7e71bac8a5f
Jongmin Won <jacobjwon@gmail.com>
2018-01-10 18:07:13 -0500

    Fix auth issue

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -27,7 +27,7 @@ class SessionForm extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     const user = this.state;
-    this.props.processForm({user});
+    this.props.processForm(user);
   }
 
   componentWillReceiveProps (nextProps) {
